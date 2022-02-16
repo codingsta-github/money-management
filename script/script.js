07015176663a64031af0802694660d245f8bb3f3
coding-sta <personal.sta.bd@gmail.com>
2022-02-17 04:29:39 +0600

    nt done

---
+++ script/script.js
@@ -0,0 +1,8 @@
+document.getElementById('calculate-btn').addEventListener('click',function(){
+const income=parseFloat(document.getElementById('income-input').value);
+const food=parseFloat(document.getElementById('food-input').value);
+const rent=parseFloat(document.getElementById('rent-input').value);
+const cloth=parseFloat(document.getElementById('cloth-input').value);
+const expense=food+rent+cloth;
+const totalExpens
+})
