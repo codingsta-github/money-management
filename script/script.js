a656d3a37286e99f18fb259ec6a47f115958fa2f
coding-sta <personal.sta.bd@gmail.com>
2022-02-17 20:48:00 +0600

    issue

--- script/script.js
+++ script/script.js
@@ -11,34 +11,39 @@ function inputValue(src) {
         const food=inputValue("food");
         const rent=inputValue("rent");
         const cloth=inputValue("cloth");
-        if (isNaN(income)) {
+        if(!isNaN(income) && income>=0 && !isNaN(food) && food>=0 && !isNaN(rent) && rent>=0 && !isNaN(cloth) && cloth>=0){
+            const totalExpense=food+rent+cloth;
+            const balance=income-totalExpense;
+            
+            document.getElementById('total-expense').innerText=totalExpense;
+            document.getElementById('balance').innerText=balance;
+            }
+        if (isNaN(income)||income<0) {
             document.getElementById('income-err').style.display='block' ;
         }
-        else if (isNaN(food)) {
+        if (isNaN(food)||food<0) {
             document.getElementById('food-err').style.display='block' ;
         }
-        else if (isNaN(rent)) {
+        if (isNaN(rent)||rent<0) {
             document.getElementById('rent-err').style.display='block' ;
         }
-        else if (isNaN(cloth)){
+        if (isNaN(cloth)||cloth<0){
             document.getElementById('cloth-err').style.display='block' ;
         }  
-        else {
-        const totalExpense=food+rent+cloth;
-        var balance=income-totalExpense;
-        
-        document.getElementById('total-expense').innerText=totalExpense;
-        document.getElementById('balance').innerText=balance;
-        }
+         
         })
 
 
 
  document.getElementById('saving-btn').addEventListener('click',function(){
      
-        const balance=document.getElementById('balance').innerText;
+        const balance=parseFloat(document.getElementById('balance').innerText);
         const save=parseFloat(document.getElementById('saving-input').value);
-        const saved=balance*(save/100);
-        document.getElementById('saving-amount').innerText=saved;
-        document.getElementById('remaining-balance').innerText=balance-saved;
+        if (balance>=0) {
+            const saved=balance*(save/100);
+            document.getElementById('saving-amount').innerText=saved;
+            document.getElementById('remaining-balance').innerText=balance-saved;
+        } else {
+            document.getElementById('saving-err').style.display='block' ;
+        }
  })
